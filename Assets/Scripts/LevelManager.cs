24e6f9e8f0337e24d9be11665997b81f1d39f41f
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-10-27 13:28:14 +0200

    1 push
    Working:
    - player controller
    - double jump
    - dash
    - killing and respawning player
    - checkpoints

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+    private PlayerController player;
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    public float respawnDelay;
+    private float gravityStore;
+
+	void Start ()
+    {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void RespawnPlayer()
+    {
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+
+
+}
